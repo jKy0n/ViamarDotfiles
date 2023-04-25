fc4facedb122ebe83cddb0340f8361923f675e48
jKyon <jk.1989@hotmail.com>
2023-04-25 17:09:23 -0300

    dwm: ~ tags & rules | dwmBlks: + icons | minors ~

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-picom --experimental-backend &
-sleep 1 && /usr/libexec/polkit-gnome-authentication-agent-1 &
-nice -n 15 dwmblocks &
 feh --bg-scale ~/Pictures/WallPapers/Andromeda.jpg 
+picom --experimental-backend &
+nice -n 15 dwmblocks &
+sleep 1 && /usr/libexec/polkit-gnome-authentication-agent-1 &
